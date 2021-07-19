5e103d092b7b9af9d28ac54f2db0a200d9897b49
Nicholas Enright <NEnright1999@gmail.com>
2021-07-19 16:31:10 -0400

    Added comment class

--- 24HourAssignment.Data/Entities/Comment.cs
+++ 24HourAssignment.Data/Entities/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,8 @@ namespace _24HourAssignment.Data.Entities
 {
     class Comment
     {
+        [ForeignKey(nameof(Comment))]
+        [Required]
+        public int ID { get; set; }
     }
 }
